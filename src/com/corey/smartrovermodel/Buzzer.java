75c997b951924726f4f76126d64a332b402e9bb4
corey <corey@DESKTOP-COCO>
2021-12-07 15:30:42 -0500

    update

--- src/com/corey/smartrovermodel/Buzzer.java
+++ src/com/corey/smartrovermodel/Buzzer.java
@@ -21,15 +21,36 @@ public class Buzzer {
 		System.out.println( "Buzzer - FullSystemCheck OK");
 	}
 	public static void getMaintenanceStatus() {
-		System.out.println( "Buzzer - Status Updated");
-	}		
+		System.out.println( "Buzzer - Maintenance Status set to XXX");
+	}
+	public static void getPowerStatus() {
+		System.out.println( "Buzzer - Power Status set to XXX");
+	}	
+	public static void getSound() {
+		System.out.println( "Buzzer - Sound set to XXX");
+	}	
+	public static void getVolume() {
+		System.out.println( "Buzzer - Volume set to XXX");
+	}	
+	public static void getBuzzerPin() {
+		System.out.println( "Buzzer - Buzzer Pin set to XXX");
+	}	
 	public static void playSound() {
-		System.out.println( "Buzzer - Playing Sound");
+		System.out.println( "Buzzer - Playing Sound XXX");
 	}	
 	public static void changeSound() {
-		System.out.println( "Buzzer - Sound Changed");
+		System.out.println( "Buzzer - Sound Changed to XXX");
 	}		
 	public static void changeVolume() {
-		System.out.println( "Buzzer - Volume Changed");
+		System.out.println( "Buzzer - Volume Changed to XXX");
 	}	
+	public static void updateMaintenanceStatus() {
+		System.out.println( "Buzzer - Maintenance Status updated to XXX");
+	}
+	public static void updatePowerStatus() {
+		System.out.println( "Buzzer - Power Status updated to XXX");
+	}
+	public static void updateBuzzerPin() {
+		System.out.println( "Buzzer - BuzzerPin updated to XXX");
+	}
 }
