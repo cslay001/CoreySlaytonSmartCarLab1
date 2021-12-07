ba40c7a2edbd4cbddd716ae92eee4a68b10ba633
corey <corey@DESKTOP-COCO>
2021-12-07 15:01:20 -0500

    update

---
+++ src/com/corey/smartrovermodel/ECU.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package com.corey.smartrovermodel;
+
+/**
+ * @author corey
+ *
+ */
+public class ECU {
+
+	private String maintenanceStatus;
+	private String powerStatus;
+
+	
+	
+	public static void getFullSystemCheck() {
+		System.out.println( "ECU - FullSystemCheck set to OK");
+	}
+	public static void getMaintenanceStatus() {
+		System.out.println( "ECU - Maintenance Status set to XXX");
+	}		
+	public static void getPowerStatus() {
+		System.out.println( "ECU - Power Status set to XXX");
+	}	
+	public static void updateMaintenanceStatus() {
+		System.out.println( "ECU - Maintenance Status updated to XXX");
+	}		
+	public static void updatePowerStatus() {
+		System.out.println( "ECU - Power Status updated to XXX");
+	}	
+}
