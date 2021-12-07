e0803efa3da75ae74930ae851173554cd13423f0
corey <corey@DESKTOP-COCO>
2021-12-07 15:04:00 -0500

    update

---
+++ src/com/corey/smartrovermodel/SmartRoverApp.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package com.corey.smartrovermodel;
+
+/**
+ * @author corey
+ *
+ */
+public class SmartRoverApp {
+
+	private String maintenanceStatus;
+	private String powerStatus;
+
+	
+	
+	public static void getFullSystemCheck() {
+		System.out.println( "SmartRoverApp - FullSystemCheck set to OK");
+	}
+	public static void getMaintenanceStatus() {
+		System.out.println( "SmartRoverApp - Maintenance Status set to XXX");
+	}		
+	public static void getPowerStatus() {
+		System.out.println( "SmartRoverApp - Power Status set to XXX");
+	}	
+	public static void updateMaintenanceStatus() {
+		System.out.println( "SmartRoverApp - Maintenance Status updated to XXX");
+	}		
+	public static void updatePowerStatus() {
+		System.out.println( "SmartRoverApp - Power Status updated to XXX");
+	}	
+}
