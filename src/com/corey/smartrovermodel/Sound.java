d3ee57b1c6d6a4884cee17103ec76e21bd126387
corey <corey@DESKTOP-COCO>
2021-12-07 15:52:32 -0500

    update

---
+++ src/com/corey/smartrovermodel/Sound.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package com.corey.smartrovermodel;
+
+/**
+ * @author corey
+ *
+ */
+public class Sound {
+
+	private String maintenanceStatus;
+	private String fileLocation;
+	private String fileName;
+	private String fileSize;
+	private String soundSelected;
+
+	
+	
+	public static void getFullSystemCheck() {
+		System.out.println( "Sound - FullSystemCheck set to OK");
+	}
+	public static void getMaintenanceStatus() {
+		System.out.println( "Sound - Maintenance Status set to XXX");
+	}		
+	public static void saveSound() {
+		System.out.println( "Sound - Sound saved to XXX");
+	}	
+	public static void deleteSound() {
+		System.out.println( "Sound - Sound deleted from XXX");
+	}
+	public static void selectSound() {
+		System.out.println( "Sound - Sound selected from XXX");
+	}
+	public static void deselectSound() {
+		System.out.println( "Sound - Sound deselected from XXX");
+	}
+	public static void updateMaintenanceStatus() {
+		System.out.println( "Sound - Maintenance Status updated to XXX");
+	}		
+	public static void updatePowerStatus() {
+		System.out.println( "Sound - Power Status updated to XXX");
+	}	
+}
