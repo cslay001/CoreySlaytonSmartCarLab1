d6e501b47f80d752dc423b1d3798ad5c58ad6c38
corey <corey@DESKTOP-COCO>
2021-12-07 14:59:17 -0500

    updated

---
+++ src/com/corey/smartrovermodel/ADC.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package com.corey.smartrovermodel;
+
+/**
+ * @author corey
+ *
+ */
+public class ADC {
+
+	private String maintenanceStatus;
+	private String powerStatus;
+
+	
+	
+	public static void getFullSystemCheck() {
+		System.out.println( "ADC - FullSystemCheck set to OK");
+	}
+	public static void getMaintenanceStatus() {
+		System.out.println( "ADC - Maintenance Status set to XXX");
+	}		
+	public static void getPowerStatus() {
+		System.out.println( "ADC - Power Status set to XXX");
+	}	
+	public static void updateMaintenanceStatus() {
+		System.out.println( "ADC - Maintenance Status updated to XXX");
+	}		
+	public static void updatePowerStatus() {
+		System.out.println( "ADC - Power Status updated to XXX");
+	}	
+}
